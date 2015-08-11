1b2039de470cee60336e2b9829c8196df5fc318a
Robin <loveingenioustech@gmail.com>
2015-08-11 14:33:37 +0800

    commit 6

---
+++ java-demo/src/main/java/demo/pattern/ConsoleLogging.java
@@ -0,0 +1,12 @@
+package demo.pattern;
+
+public class ConsoleLogging implements Logging
+{
+
+    @Override
+    public void write(final String message)
+    {
+        System.out.println(message);
+    }
+
+}
---
+++ java-demo/src/main/java/demo/pattern/Logging.java
@@ -0,0 +1,16 @@
+package demo.pattern;
+
+/**
+ * 策略模式
+ * 
+ * 类名称：Logging   
+ * 类描述：   
+ * 创建时间：2015-8-11 下午2:29:24   
+ * 修改时间：2015-8-11 下午2:29:24   
+ * 修改备注：   
+ * @version
+ */
+public interface Logging
+{
+    void write(String message);
+}
