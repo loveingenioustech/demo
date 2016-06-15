dea442f3884dcc55a7b91b1599519ae2e83ce626
Robin <loveingenioustech@gmail.com>
2016-06-15 14:28:20 +0800

    commit 47

--- java-demo/src/main/java/demo/collection/ArrayDemo.java
+++ java-demo/src/main/java/demo/collection/ArrayDemo.java
@@ -13,10 +13,58 @@ public class ArrayDemo
         demoInt();
 
         demoCopy();
-        
+
         demoSort();
 
+        demoPair();
+
         demoGrow();
+    }
+
+    /**
+     * Gets the minimum and maximum of an array of strings.
+     * 
+     * @param a
+     *            an array of strings
+     * @return a pair with the min and max value, or null if a is null or empty
+     */
+    public static Pair<String> minmax(String[] a)
+    {
+        if (a == null || a.length == 0)
+            return null;
+        String min = a[0];
+        String max = a[0];
+        for (int i = 1; i < a.length; i++)
+        {
+            if (min.compareTo(a[i]) > 0)
+                min = a[i];
+            if (max.compareTo(a[i]) < 0)
+                max = a[i];
+        }
+        return new Pair<String>(min, max);
+    }
+
+    /**
+     * Gets the minimum and maximum of an array of objects of type T.
+     * 
+     * @param a
+     *            an array of objects of type T
+     * @return a pair with the min and max value, or null if a is null or empty
+     */
+    public static <T extends Comparable> Pair<T> minmax(T[] a)
+    {
+        if (a == null || a.length == 0)
+            return null;
+        T min = a[0];
+        T max = a[0];
+        for (int i = 1; i < a.length; i++)
+        {
+            if (min.compareTo(a[i]) > 0)
+                min = a[i];
+            if (max.compareTo(a[i]) < 0)
+                max = a[i];
+        }
+        return new Pair<T>(min, max);
     }
 
     /**
@@ -127,6 +175,14 @@ public class ArrayDemo
             System.out.println("name=" + e.getName() + ",salary=" + e.getSalary());
         }
 
+    }
+
+    private static void demoPair()
+    {
+        String[] words = { "Mary", "had", "a", "little", "lamb" };
+        Pair<String> mm = minmax(words);
+        System.out.println("min = " + mm.getFirst());
+        System.out.println("max = " + mm.getSecond());
     }
 
 }
