28014ad7383328ff90c2af1133222e0e5b006791
Robin <loveingenioustech@gmail.com>
2016-06-17 17:51:36 +0800

    commit 50

--- java-demo/src/main/java/demo/io/Employee.java
+++ java-demo/src/main/java/demo/io/Employee.java
@@ -1,12 +1,28 @@
 package demo.io;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.StringTokenizer;
+
 public class Employee
 {
     private String name;
     private int age;
+    private double salary;
+    private Date hireDay;
+
     public static final int len = 8;// String 型的定长，相当于数据库中的类型长度
 
-    public Employee(String name, int age)
+    public Employee()
+    {
+
+    }
+
+    public Employee(String name, int age, double salary, int year, int month, int day)
     {
         System.out.println(name + ": " + name.getBytes().length + " : " + name.length());
 
@@ -26,6 +42,46 @@ public class Employee
 
         // 设置年龄
         this.age = age;
+
+        // 设置薪水
+        this.salary = salary;
+
+        // 设置入职日期
+        GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
+        this.hireDay = calendar.getTime();
+    }
+
+    /**
+     * Writes employee data to a print writer
+     * 
+     * @param out
+     *            the print writer
+     */
+    public void writeData(PrintWriter out) throws IOException
+    {
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTime(hireDay);
+        out.println(name + "|" + salary + "|" + calendar.get(Calendar.YEAR) + "|" + (calendar.get(Calendar.MONTH) + 1)
+                + "|" + calendar.get(Calendar.DAY_OF_MONTH));
+    }
+
+    /**
+     * Reads employee data from a buffered reader
+     * 
+     * @param in
+     *            the buffered reader
+     */
+    public void readData(BufferedReader in) throws IOException
+    {
+        String s = in.readLine();
+        StringTokenizer t = new StringTokenizer(s, "|");
+        name = t.nextToken();
+        salary = Double.parseDouble(t.nextToken());
+        int y = Integer.parseInt(t.nextToken());
+        int m = Integer.parseInt(t.nextToken());
+        int d = Integer.parseInt(t.nextToken());
+        GregorianCalendar calendar = new GregorianCalendar(y, m - 1, d);
+        hireDay = calendar.getTime();
     }
 
     /**
@@ -60,12 +116,38 @@ public class Employee
         this.age = age;
     }
 
+    public double getSalary()
+    {
+        return salary;
+    }
+
+    public void setSalary(double salary)
+    {
+        this.salary = salary;
+    }
+
+    public Date getHireDay()
+    {
+        return hireDay;
+    }
+
+    public void setHireDay(Date hireDay)
+    {
+        this.hireDay = hireDay;
+    }
+
     /**
      * @return the len
      */
     public static int getLen()
     {
         return len;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Employee [name=" + name + ", age=" + age + ", salary=" + salary + ", hireDay=" + hireDay + "]";
     }
 
 }
---
+++ java-demo/src/main/java/demo/io/IODemo.java
@@ -0,0 +1,113 @@
+package demo.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class IODemo
+{
+
+    public static void main(String[] args)
+    {
+        demoWrite();
+
+        demoRead();
+    }
+
+    private static void demoWrite()
+    {
+        Employee[] staff = new Employee[3];
+
+        staff[0] = new Employee("Carl Cracker", 30, 75000, 1987, 12, 15);
+        staff[1] = new Employee("Harry Hacker", 28, 50000, 1989, 10, 1);
+        staff[2] = new Employee("Tony Tester", 16, 40000, 1990, 3, 15);
+
+        try
+        {
+            // save all employee records to the file employee.dat
+            PrintWriter out = new PrintWriter(new FileWriter("employee.dat"));
+            writeData(staff, out);
+            out.close();
+
+            // retrieve all records into a new array
+            BufferedReader in = new BufferedReader(new FileReader("employee.dat"));
+            Employee[] newStaff = readData(in);
+            in.close();
+
+            // print the newly read employee records
+            for (Employee e : newStaff)
+            {
+                System.out.println(e);
+            }
+
+        }
+        catch (IOException exception)
+        {
+            exception.printStackTrace();
+        }
+
+    }
+
+    private static void demoRead()
+    {
+        try
+        {
+            // retrieve all records into a new array
+            BufferedReader in = new BufferedReader(new FileReader("employee.dat"));
+            Employee[] newStaff = readData(in);
+            in.close();
+
+            // print the newly read employee records
+            for (Employee e : newStaff)
+            {
+                System.out.println(e);
+            }
+
+        }
+        catch (IOException exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * Writes all employees in an array to a print writer
+     * 
+     * @param employees
+     *            an array of employees
+     * @param out
+     *            a print writer
+     */
+    static void writeData(Employee[] employees, PrintWriter out) throws IOException
+    {
+        // write number of employees
+        out.println(employees.length);
+
+        for (Employee e : employees)
+            e.writeData(out);
+    }
+
+    /**
+     * Reads an array of employees from a buffered reader
+     * 
+     * @param in
+     *            the buffered reader
+     * @return the array of employees
+     */
+    static Employee[] readData(BufferedReader in) throws IOException
+    {
+        // retrieve the array size
+        int n = Integer.parseInt(in.readLine());
+
+        Employee[] employees = new Employee[n];
+        for (int i = 0; i < n; i++)
+        {
+            employees[i] = new Employee();
+            employees[i].readData(in);
+        }
+        
+        return employees;
+    }
+}
