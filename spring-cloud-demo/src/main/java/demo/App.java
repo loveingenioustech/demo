2a365de166c7a84469b35dd6deedf14d87ca07ca
Robin Long <loveingenioustech@gmail.com>
2017-07-25 11:47:44 +0800

    start spring boot, 9985

---
+++ spring-cloud-demo/src/main/java/demo/App.java
@@ -0,0 +1,16 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        // 在main 方法里启动应用程序
+        SpringApplication.run(App.class, args);
+    }
+}
