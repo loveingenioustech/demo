59e855e8f01aa00f5609a3e150768141aa4d0faf
Robin Long <loveingenioustech@gmail.com>
2017-08-09 14:47:05 +0800

    ASM,9980

---
+++ java-demo/src/main/java/demo/asm/Account.java
@@ -0,0 +1,11 @@
+package demo.asm;
+
+/**
+ * Created by Administrator on 2017-08-09.
+ */
+public class Account {
+    public void operation() {
+        System.out.println("operation....");
+    }
+
+}
