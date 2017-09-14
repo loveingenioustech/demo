3386f5ec770750ede4619f0f01c49ff47070e170
Robin Long <loveingenioustech@gmail.com>
2017-09-14 14:20:31 +0800

    appfuse, 9939

---
+++ appfuse-demo/src/main/java/demo/service/impl/LookupManagerImpl.java
@@ -0,0 +1,37 @@
+package demo.service.impl;
+
+import demo.dao.LookupDao;
+import demo.model.LabelValue;
+import demo.model.Role;
+import demo.service.LookupManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Implementation of LookupManager interface to talk to the persistence layer.
+ *
+ * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
+ */
+@Service("lookupManager")
+public class LookupManagerImpl implements LookupManager {
+    @Autowired
+    LookupDao dao;
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<LabelValue> getAllRoles() {
+        List<Role> roles = dao.getRoles();
+        List<LabelValue> list = new ArrayList<LabelValue>();
+
+        for (Role role1 : roles) {
+            list.add(new LabelValue(role1.getName(), role1.getName()));
+        }
+
+        return list;
+    }
+}
---
+++ appfuse-demo/src/main/java/demo/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package demo.service.impl;
+
+import demo.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
